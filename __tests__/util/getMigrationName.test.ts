86a0a512c9a813ee4b3ff9aa561645c79c3f2cc2
anthony sottile <anthony.sottile@sentry.io>
2025-04-29 17:08:43 -0400

    show output from multiple migrations

--- __tests__/util/getMigrationName.test.ts
+++ __tests__/util/getMigrationName.test.ts
@@ -1,43 +1,52 @@
-import {getMigrationName} from '@app/util/getMigrationName';
+import {getMigrationNames} from '@app/util/getMigrationName';
 
 test('handles garbage input', () => {
-  const [app, migration] = getMigrationName('', 'sentry');
+  const [[app, migration]] = [...getMigrationNames('', 'sentry')];
   expect(migration).toBe('');
   expect(app).toBe('sentry')
 });
 
 test('gets filename without extension', () => {
-  const [app, migration] = getMigrationName('./src/sentry/migrations/001_testing.py', 'notused');
+  const [[app, migration]] = [...getMigrationNames('./src/sentry/migrations/001_testing.py', 'notused')];
   expect(migration).toBe('001_testing');
   expect(app).toBe('sentry');
 });
 
 test('gets filename when passed only filename with extension', () => {
-  const [app, migration] = getMigrationName('001_testing.py', 'sentry');
+  const [[app, migration]] = [...getMigrationNames('001_testing.py', 'sentry')];
   expect(migration).toBe('001_testing');
   expect(app).toBe('sentry');
 });
 
 test('gets filename without extension', () => {
-  const [app, migration] = getMigrationName('./src/sentry/migrations/001_testing.py', 'notused');
+  const [[app, migration]] = [...getMigrationNames('./src/sentry/migrations/001_testing.py', 'notused')];
   expect(migration).toBe('001_testing');
   expect(app).toBe('sentry')
 });
 
 test('gets filename with partial path', () => {
-  const [app, migration] = getMigrationName('migrations/001_testing.py', 'sentry');
+  const [[app, migration]] = [...getMigrationNames('migrations/001_testing.py', 'sentry')];
   expect(migration).toBe('001_testing');
   expect(app).toBe('sentry');
 });
 
 test('gets application names from nested apps', () => {
-  const [app, migration] = getMigrationName('./src/sentry/feedback/migrations/001_testing.py', 'notused');
+  const [[app, migration]] = [...getMigrationNames('./src/sentry/feedback/migrations/001_testing.py', 'notused')];
   expect(migration).toBe('001_testing');
   expect(app).toBe('feedback')
 });
 
 test('preserve single quotes (e.g. if used to escape)', () => {
-  const [app, migration] = getMigrationName(`'foo/migrations/001_testing.py'`, 'notused');
+  const [[app, migration]] = [...getMigrationNames(`'foo/migrations/001_testing.py'`, 'notused')];
   expect(migration).toBe(`'001_testing'`);
   expect(app).toBe('foo');
+});
+
+test('handles multiple lines', () => {
+    const input = `
+src/sentry/migrations/0001_migration.py
+src/sentry/feedback/migrations/0002_migration2.py
+`;
+    const ret = [...getMigrationNames(input, 'sentry')];
+    expect(ret).toEqual([['sentry', '0001_migration'], ['feedback', '0002_migration2']]);
 });
--- src/util/getMigrationName.ts
+++ src/util/getMigrationName.ts
@@ -1,27 +1,29 @@
 type MigrationName = [appLabel: string, name: string];
 
-export function getMigrationName(name: string, fallbackApp: string): MigrationName {
-  const parts = name.split('/');
-  const relevantParts = parts.slice(-3);
+export function* getMigrationNames(name: string, fallbackApp: string): Generator<MigrationName> {
+  for (const part of name.trim().split(/\s+/)) {
+    const parts = part.split('/');
+    const relevantParts = parts.slice(-3);
 
-  // Not enough paths segments to determine the app name.
-  if (relevantParts.length < 3) {
     const migrationName = stripExtension(relevantParts.pop() ?? '');
-    return [fallbackApp, migrationName];
-  }
 
-  const migrationName = stripExtension(relevantParts.pop() ?? '');
-  let appName = fallbackApp;
-  if (relevantParts[relevantParts.length - 1] === 'migrations') {
-    appName = relevantParts[relevantParts.length - 2] ?? fallbackApp;
-  } else {
-    appName = relevantParts.pop() ?? fallbackApp;
-  }
-  if (appName && appName.startsWith("'")) {
-    appName = appName.slice(1);
-  }
+    // Not enough paths segments to determine the app name.
+    if (relevantParts.length < 2) {
+      yield [fallbackApp, migrationName];
+    } else {
+      let appName = fallbackApp;
+      if (relevantParts[relevantParts.length - 1] === 'migrations') {
+        appName = relevantParts[relevantParts.length - 2] ?? fallbackApp;
+      } else {
+        appName = relevantParts.pop() ?? fallbackApp;
+      }
+      if (appName && appName.startsWith("'")) {
+        appName = appName.slice(1);
+      }
 
-  return [appName, migrationName];
+      yield [appName, migrationName];
+    }
+  }
 }
 
 const quotePattern = /(.*?)([']?)$/;
